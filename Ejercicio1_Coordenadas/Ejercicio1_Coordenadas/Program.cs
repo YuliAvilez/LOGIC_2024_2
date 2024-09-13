9ea785a9da07ce5d5d90d3e3642cb549493fddf2
YULI AVILEZ GAZCON <yuliavilezgazcon@gmail.com>
2024-09-13 16:58:37 -0500

    Se empieza con el desarrollo del codigo

---
+++ Ejercicio1_Coordenadas/Ejercicio1_Coordenadas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        // Aqui van las lineas del codigo
+        double x1, y1, x2, y2, d;
+
+
+        Console.Write("Ingrese el valor de x1: ");
+        x1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese el valor de x2: ");
+        x2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese el valor de y1: ");
+        y1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese el valor de y2: ");
+        y2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+    }
+}
