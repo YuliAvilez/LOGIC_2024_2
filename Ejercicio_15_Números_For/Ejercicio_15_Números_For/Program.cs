593aa16b74b64ea3843106860044cfb64a028687
YULI AVILEZ GAZCON <yuliavilezgazcon@gmail.com>
2024-09-27 20:56:56 -0500

    Se empieza el código

---
+++ Ejercicio_15_Números_For/Ejercicio_15_Números_For/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+      /*15.	Programe un algoritmo que pida a usuario 2 números por pantalla y que muestre todos los números comprendidos entre ambos números incluyéndolos.*/
+
+        //Datos de entrada
+        int num1, num2;
+
+        Console.WriteLine("Ingrese el primer número: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+    }
+}
